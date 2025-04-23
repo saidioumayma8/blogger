5e8435dfa20f6e2b0dbea3e8b85c9c152937ed29
Oumayma Saidi <oumaymasaidi908@gmail.com>
2025-04-23 17:11:18 +0100

    login

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app.routes';
+import { LoginComponent } from './app/components/pages/login/login.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(LoginComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes))
+  ]
+}).catch(err => console.error(err));
